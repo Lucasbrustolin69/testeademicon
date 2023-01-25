ef243f84255cde829f891cc7e6c61de8be00e0ee
Lucas Brustolin <lucaasbrustolin@gmail.com>
2023-01-25 12:53:59 -0300

    Alterando novos vendedores

--- src/models/vendedormodel.js
+++ src/models/vendedormodel.js
@@ -4,12 +4,15 @@ const Schema = mongoose.Schema
 const ObjectId = Schema.ObjectId
 
 const vendedorSchema = new Schema({
-    id: ObjectId,
-    nome: String,
-    contratado: String,
-    comissao: String,
-   entrada: String,
-   saida: String
+
+   id: ObjectId,
+   nome: String,
+   contratado: Date,
+   comissao: Number,
+   horariodetrabalho: {
+        entrada: String,
+        saida: String
+    }
 })
 
 const vendedorModel = mongoose.model('vendedor', vendedorSchema)
